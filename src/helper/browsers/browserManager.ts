bfaa0245b9483e40054aea99058c71baedb238a9
Vishal <vishal.bhatt8807@gmail.com>
2024-11-26 09:39:35 +0530

    Add Logic to run in MSEdge

--- src/helper/browsers/browserManager.ts
+++ src/helper/browsers/browserManager.ts
@@ -3,8 +3,13 @@ import { LaunchOptions, chromium, firefox, webkit } from "@playwright/test";
 const options: LaunchOptions = {
     headless: !true
 }
+const edgeOptions: LaunchOptions ={
+    headless: !true,
+    
+    channel: 'msedge',
+}
 export const invokeBrowser = () => {
-    const browserType = process.env.npm_config_BROWSER || "chrome";
+    const browserType = process.env.npm_config_BROWSER || "edge";
     switch (browserType) {
         case "chrome":
             return chromium.launch(options);
@@ -12,6 +17,8 @@ export const invokeBrowser = () => {
             return firefox.launch(options);
         case "webkit":
             return webkit.launch(options);
+        case "edge":
+            return chromium.launch(edgeOptions);
         default:
             throw new Error("Please set the proper browser!")
     }
